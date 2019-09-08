b88a9051d96624eaf3d5228ba818db0a459ffa32
Sainath Adapa <adapasainath@gmail.com>
2019-09-08 15:27:52 +0200

    added mixup code

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
-import pickle, os
+import os
 import numpy as np
-import pandas as pd
 import argparse
 
 import torch
@@ -13,11 +12,24 @@ from model import MirexModel
 from config import CONFIG
 
 
-def train(out_dir, inp_txt, num_threads, task, batch_size=4):
-    
-    melspec_dir = os.path.normpath(out_dir)+ '/melspec'
+# from https://github.com/tugstugi/pytorch-speech-commands/blob/master/mixup.py
+def mixup_cross_entropy_loss(inp, target, size_average=True):
+    """Origin: https://github.com/moskomule/mixup.pytorch
+    in PyTorch's cross entropy, targets are expected to be labels
+    so to predict probabilities this loss is needed
+    suppose q is the target and p is the inp
+    """
+    assert inp.size() == target.size()
+    inp = torch.log(torch.nn.functional.softmax(inp, dim=1).clamp(1e-5, 1))
+    loss = -torch.sum(inp * target)
+    return loss / inp.size()[0] if size_average else loss
 
-    #create a model directory
+
+def train(out_dir, inp_txt, num_threads, task, batch_size=4):
+
+    melspec_dir = os.path.normpath(out_dir) + '/melspec'
+
+    # create a model directory
     model_dir = os.path.normpath(out_dir) + '/' + 'model'
     os.makedirs(model_dir, exist_ok=True)
 
@@ -26,19 +38,20 @@ def train(out_dir, inp_txt, num_threads, task, batch_size=4):
     train_dataset = AudioDataset(train_fnames, train_labels, melspec_dir)
     val_dataset = AudioDataset(val_fnames, val_labels, melspec_dir, train=False)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    train_loader_1 = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    train_loader_2 = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size)
 
     num_classes = CONFIG[task]['num_classes']
+
     model = MirexModel(num_classes)
-    print(model)
 
     # Define optimizer, scheduler and loss criteria
     optimizer = optim.Adam(model.parameters(), lr=0.001, amsgrad=True)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, verbose=True)
     criterion = nn.CrossEntropyLoss()
 
-    cuda=False
+    cuda = False
     device = torch.device('cuda:0' if cuda else 'cpu')
     print('Device: ', device)
     model = model.to(device)
@@ -52,7 +65,23 @@ def train(out_dir, inp_txt, num_threads, task, batch_size=4):
     for i in range(epochs):
 
         this_epoch_train_loss = 0
-        for inputs, labels in train_loader:
+        for i1, i2 in zip(train_loader_1, train_loader_2):
+
+            # mixup---------
+            x1, y1 = i1
+            x2, y2 = i2
+
+            alpha = 1
+            mixup_vals = np.random.beta(alpha, alpha, i1[0].shape[0])
+
+            mvals = torch.Tensor(mixup_vals.reshape(mixup_vals.shape[0], 1, 1, 1))
+            inputs = (mvals * x1) + ((1 - mvals) * x2)
+
+            y1_onehot = torch.nn.functional.one_hot(y1, num_classes).float()
+            y2_onehot = torch.nn.functional.one_hot(y2, num_classes).float()
+            mvals = torch.Tensor(mixup_vals.reshape(mixup_vals.shape[0], 1))
+            labels = (mvals * y1_onehot) + ((1 - mvals) * y2_onehot)
+            # mixup ends ----------
 
             inputs = inputs.to(device)
             labels = labels.to(device)
@@ -61,7 +90,7 @@ def train(out_dir, inp_txt, num_threads, task, batch_size=4):
             with torch.set_grad_enabled(True):
                 model = model.train()
                 outputs = model(inputs)
-                loss = criterion(outputs, labels)
+                loss = mixup_cross_entropy_loss(outputs, labels, False)
                 loss.backward()
                 optimizer.step()
                 this_epoch_train_loss += loss.detach().cpu().numpy()
@@ -78,7 +107,7 @@ def train(out_dir, inp_txt, num_threads, task, batch_size=4):
                 loss = criterion(outputs, labels)
                 this_epoch_valid_loss += loss.detach().cpu().numpy()
 
-        this_epoch_train_loss /= len(train_loader)
+        this_epoch_train_loss /= len(train_loader_1)
         this_epoch_valid_loss /= len(val_loader)
 
         train_loss_hist.append(this_epoch_train_loss)
@@ -99,12 +128,13 @@ def train(out_dir, inp_txt, num_threads, task, batch_size=4):
         scheduler.step(this_epoch_valid_loss)
 
 
-if __name__=="__main__":
-    parser  = argparse.ArgumentParser()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--scratch', help='Path to scratch folder')
     parser.add_argument('-i', '--input_file', help='ASCII text file with train labels')
     parser.add_argument('-n', '--num_threads', type=int, default=4, help='Num of threads to use')
-    parser.add_argument('-t', '--task', type=str, default='kpop_mood', help='Task name, see config for choices')
+    parser.add_argument('-t', '--task', type=str, default='kpop_mood',
+                        help='Task name, see config for choices')
 
     args = parser.parse_args()
     train(args.scratch, args.input_file, args.num_threads, args.task)
